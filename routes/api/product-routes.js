6b16a184d7c600ce48a3963428c05af419c9802a
Sherman Burwell <shermanburwell3@gmail.com>
2024-08-12 13:09:03 -0500

    Added code to delete route for product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -94,6 +94,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((product) => {
+      if (!product) {
+        res.status(404).json({ message: 'No product found with this id' });
+        return;
+      }
+      res.status(200).json(product);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
