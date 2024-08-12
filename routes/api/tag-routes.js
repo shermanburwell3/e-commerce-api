3173613f3612872e18c0ad27746031c356c4fcd9
Sherman Burwell <shermanburwell3@gmail.com>
2024-08-12 13:39:53 -0500

    Added code to routes for tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,14 +3,37 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+router.get('/', async (req, res) => {
+  // Find all tags
+  try {
+    const tags = await Tag.findAll({
+      include: Product
+    });
+
+    res.status(200).json(tags);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Server Error' });
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+router.get('/:id', async (req, res) => {
+  // Find single tag by its id
+  try {
+    const tag = await Tag.findOne({
+      where: { id: req.params.id },
+      include: Product
+    });
+
+    if (!tag) {
+      return res.status(404).json({ message: 'Tag not found' });
+    }
+
+    res.status(200).json(tag);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Server Error' });
+  }
 });
 
 router.post('/', (req, res) => {
