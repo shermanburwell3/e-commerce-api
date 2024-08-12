0cedbcb1fedf8c18853000cf98f396b37db7a76f
Sherman Burwell <shermanburwell3@gmail.com>
2024-08-12 12:20:26 -0500

    Added code for foreign keys in index.js in models dir

--- models/index.js
+++ models/index.js
@@ -5,12 +5,27 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: "SET NULL"
+});
 
 // Products belongToMany Tags (through ProductTag)
 
+Product.belongsToMany(ProductTag, {
+  foreginKey: "id"
+})
+
 // Tags belongToMany Products (through ProductTag)
+
+Tag.belongsToMany(Product, {
+  foreignKey: "product_id"
+});
 
 module.exports = {
   Product,
